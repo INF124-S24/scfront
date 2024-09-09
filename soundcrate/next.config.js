0e04712cf1a36aaebac2dcee41ec663ee0fdcc8a
Ashley Anyanwu <ashley0425@yahoo.com>
2024-09-09 01:17:32 -0700

    edited next config

--- soundcrate/next.config.js
+++ soundcrate/next.config.js
@@ -1,18 +1,17 @@
-// module.exports = {
-//   webpack: (config, { isServer }) => {
-//     if (!isServer) {
-//       config.resolve.fallback = {
-//         fs: false,
-//       };
-//     }
-//     return config;
-//   },
-// };
-
-const nextConfig = {
-  basePath: "/soundcrate",
-  output: "export",
-  reactStrictMode: true,
+module.exports = {
+  output: 'export', // For static export
+  images: {
+    unoptimized: true, // Disable image optimization for GitHub Pages
+  },
+  basePath: '/soundcrate', // Replace with your actual repo name
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false, // Handle 'fs' module on the client-side
+      };
+    }
+    return config;
+  },
 };
 
-module.exports = nextConfig;
+
